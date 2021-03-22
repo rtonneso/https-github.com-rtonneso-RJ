75daee7a110951a18aa71ea71d8e9cd0049c3dcd
Andrew Law <66688880+captivea-ala@users.noreply.github.com>
2021-03-22 16:12:52 -0600

    Fix Errors from Old Apps

--- to_fee_definition/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import fee_definition
-from . import product_template
-from . import product_product
--- to_fee_definition_sale/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import fee_definition
-from . import sale_order_line_fee
-from . import sale_order_line
-from . import sale_order
-from . import product_product
