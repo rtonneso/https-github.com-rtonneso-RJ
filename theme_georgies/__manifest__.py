0c502fd3ff6d183e0260f767672623cf74e2e333
Mike Drake <mike@lichen.co>
2021-03-25 12:49:46 -0400

    Starting testing theme with v14.

---
+++ theme_georgies/__manifest__.py
@@ -0,0 +1,29 @@
+{
+  'name':'Georgies.com',
+  'description': 'Georgies.com theme styling by Lichen',
+  'version':'0.9',
+  'author':'Lichen',
+
+  'data': [
+      'views/layout.xml',
+      'views/pages.xml',
+      'views/assets.xml',
+      'views/snippets.xml',
+      'views/options.xml'
+  ],
+  'images': [
+    'static/description/georgies_screenshot.jpg'
+  ],
+  'category': 'Theme',
+  'depends': [
+    'website',
+    'website_theme_install',
+    'website_sale',
+    'website_sale_comparison',
+    'website_sale_wishlist',
+    'website_blog',
+    'website_event',
+    'website_event_sale',
+    'mass_mailing'
+  ],
+}
