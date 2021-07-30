78e70b538db6a3dfaaf94d5d10ebcd46740de919
Mike Drake <mike@lichen.co>
2021-07-30 10:43:14 -0700

    Added Georgies theme to staging. Going to continue to develop and test it in this branch.

---
+++ theme_georgies/__manifest__.py
@@ -0,0 +1,28 @@
+{
+  'name':'Georgies.com',
+  'description': 'Georgies.com theme styling by Lichen',
+  'version':'0.993',
+  'author':'Lichen',
+
+  'data': [
+      'views/layout.xml',
+      'views/pages.xml',
+      'views/assets.xml',
+      'views/snippets.xml',
+      'views/options.xml'
+  ],
+  'images': [
+    'static/description/georgies_screenshot.jpg'
+  ],
+  'category': 'Theme',
+  'depends': [
+    'website',
+    'website_sale',
+    'website_sale_comparison',
+    'website_sale_wishlist',
+    'website_blog',
+    'website_event',
+    'website_event_sale',
+    'mass_mailing'
+  ],
+}
