540525e10849d2edeaae19b62493b051da961d0f
Mike Drake <mike@lichen.co>
2021-11-24 15:53:47 -0500

    Disabled top search bar placeholder switch temporarily. Updated snippets and last child paragraph margins.

--- theme_georgies/__manifest__.py
+++ theme_georgies/__manifest__.py
@@ -1,7 +1,7 @@
 {
   'name':'Georgies.com',
   'description': 'Georgies.com theme styling by Lichen',
-  'version':'1.0',
+  'version':'1.01',
   'author':'Lichen',
   'data': [
       'views/layout.xml',
