b6b3d9d0c6dbaf23e5fdf3add747aeb725ed93eb
Mike Drake <mike@lichen.co>
2022-01-13 17:59:02 -0500

    Made some adjustments to the spacing on elements of the product page.

--- theme_georgies/__manifest__.py
+++ theme_georgies/__manifest__.py
@@ -1,7 +1,7 @@
 {
   'name':'Georgies.com',
   'description': 'Georgies.com theme styling by Lichen',
-  'version':'1.05',
+  'version':'1.06',
   'author':'Lichen',
   'data': [
       'views/layout.xml',
