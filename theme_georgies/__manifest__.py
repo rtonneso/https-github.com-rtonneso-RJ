590e417c122e8b6628220f14d3a325109f51acf7
Mike Drake <mike@lichen.co>
2022-01-13 17:00:40 -0500

    Fixed a bug with the alternative products modifications.

--- theme_georgies/__manifest__.py
+++ theme_georgies/__manifest__.py
@@ -1,7 +1,7 @@
 {
   'name':'Georgies.com',
   'description': 'Georgies.com theme styling by Lichen',
-  'version':'1.04',
+  'version':'1.05',
   'author':'Lichen',
   'data': [
       'views/layout.xml',
