073a60dcf100ceb4365f50204d829de339c46d66
Mike Drake <mike@lichen.co>
2022-02-22 12:24:07 -0500

    Fixed a number of small bugs. Got search placeholder changed for product search. Ensured all the snippets look proper on different sized devices and have proper default colors. Modified terms and conditions section.

--- theme_georgies/__manifest__.py
+++ theme_georgies/__manifest__.py
@@ -1,7 +1,7 @@
 {
   'name':'Georgies.com',
   'description': 'Georgies.com theme styling by Lichen',
-  'version':'1.3',
+  'version':'1.4',
   'author':'Lichen',
   'data': [
       'views/layout.xml',
