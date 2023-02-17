b6fc7de6b53bd67e642fc4a45a3cea013d18901f
Mike Drake <mike@lichen.co>
2023-02-16 23:43:46 -0500

    Updated the product page, made changes to the checkout flow, and fixed top bar in customer portal view.

--- theme_georgies/__manifest__.py
+++ theme_georgies/__manifest__.py
@@ -2,7 +2,7 @@
   'name':'Georgies.com',
   'description': 'Georgies.com theme by Lichen',
   'category': 'Theme/Website',
-  'version':'1.9',
+  'version':'2.2',
   'author':'Lichen',
   'summary': 'Art, Ceramics',
   'sequence': 110,
